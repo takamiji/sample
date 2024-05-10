c54fb0ce8dfa459f760a002db5de7ee35b4ccd72
takamiji <mijikatsunao@gmail.com>
2024-05-10 10:05:28 +0900

    Cloud FireStoreに最初のデータを保存しよう

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../flavors.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -9,6 +10,20 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(F.title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            final firstData = {"msg": "メッセージに成功しました"};
+            await FirebaseFirestore.instance
+                .collection('data')
+                .doc()
+                .set(firstData);
+          } catch (e) {
+            print(e);
+            print('エラーが発生しました');
+          }
+        },
       ),
       body: Center(
         child: Text(
